479209de6409a5223cc996d280c44ddda8868286
jacoblc <48058513+jacoblc@users.noreply.github.com>
2019-05-25 02:40:45 -0400

    Update LoanTest.java

--- FinalExamCode/src/test/java/pkgUT/LoanTest.java
+++ FinalExamCode/src/test/java/pkgUT/LoanTest.java
@@ -10,39 +10,24 @@ import org.junit.Test;
 import pkgHelper.Loan;
 
 public class LoanTest {
-
+	
+	
+	
 	@Test
-	public void LoanTest() {
+	public void LoanTest1() {
 		
 		double loanAmount = 100000;
-		double interestRate = .07;
-		double extraPayment = 0;
-		double futureValue = 0;
+		double interestRate = 0.07;
+		int nbrOfYears = 20;
+		double additionalPayment = 100;
+		Date firstPaymentDate = Loan.parseDate("2019-05-31");
 		
-		int term = 20;
+		boolean bInterestCalc = false;
+		double futureValue=0;
 		
-		Date dueDate = Loan.parseDate("2019-07-04");
-		boolean Compound = false;
-		
-		Loan l = new Loan(loanAmount, interestRate, term, dueDate, extraPayment, futureValue, Compound);
+		Loan l = new Loan(loanAmount, interestRate, nbrOfYears, additionalPayment,firstPaymentDate,bInterestCalc,futureValue);
 	}
-	@Test
-	public void totalPaymentTest() {
-		Loan l = new Loan(1000.0, 0.07,1, Loan.parseDate("2019-07-04"),0,0,false);
-		String total =l.getTotPayment();
-		assertEquals(total,"1038.32");
-}
-	@Test
-	public void totalInterestTest() {
-		Loan l = new Loan(1000.0, 0.07,1, Loan.parseDate("2019-07-04"),0,0,false);
-		String total =l.getTotInterest();
-		assertEquals(total,"38.32");
-}
-	@Test
-	public void totalPrincipleTest() {
-		Loan l = new Loan(1000.0, 0.07,20, Loan.parseDate("2019-07-04"),0,0,false);
-		double total =l.getLoanAmount();
-		assertTrue(total == 1000.0);
-	
-}
+
+
+
 }
